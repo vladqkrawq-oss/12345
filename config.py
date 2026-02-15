439b32e0ce4f794aa191303c3c7df02384edad32
vladqkrawq-oss <vlad.qkrawq@gmail.com>
2026-02-15 20:34:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,4 @@
-# Токен бота от @BotFather (ОБЯЗАТЕЛЬНО)
-TOKEN = '8563963399:AAE9Qs5EjfC1h7qlsIXT3HynS7FFrfl0ICY'
-
-# Токен от @CryptoBot (для платежей) - если не используешь, оставь пустым
-CRYPTOPAY_API_TOKEN = '532438:AAo3dJRE2Gid4P0ljqExTm7rzv4issrEddt'
-
-# ID администратора (БЕЗ КАВЫЧЕК - просто число!)
+TOKEN = '8563963399:AAE9Bd3VUbQOhYXQnSdNrVkCHf5dw9lQ3ZU'
 ADMIN_ID = 5684982163
-
-# Название файла базы данных
-DATABASE_NAME = 'userdata.db'
-
-# Настройки магазина
-SHOP_NAME = "Мой магазин аккаунтов"
-CURRENCY = "RUB"  # Валюта для отображения цен'
+DATABASE_NAME = 'shop.db'
+CRYPTOPAY_API_TOKEN = '532438:AAo3dJRE2Gid4P0ljqExTm7rzv4issrEddt'
